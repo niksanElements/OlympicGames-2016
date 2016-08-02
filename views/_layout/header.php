25fc8dd9645104539afdc93ed56e4569dd4b9eb7
Nikolay Nikolov <niksan9411@gmail.com>
2016-08-02 23:53:39 +0300

    My First Commit

---
+++ views/_layout/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Olympic Games 2016</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <link rel="stylesheet" href="content/styles/layout.css" type="text/css" />
+</head>
+<body id="top">
+<div class="wrapper col1">
+    <div id="header">
+        <div class="fl_left">
+            <h1><a href="index.html">Olympic Games 2016</a></h1>
+        </div>
+        <div class="fl_right">
+            <a href="#">Login</a>
+            <a href="#">Registrete</a>
+        </div>
+        <br class="clear" />
+    </div>
+</div>
+
+<?php require_once('show-notify-messages.php'); ?>
