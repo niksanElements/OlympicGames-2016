8acaee073692396cb5fdd20b4ef56ed082575862
stefan-delchev <s.p.delchev@gmail.com>
2016-08-23 11:12:47 +0300

    Edit search

--- models/SearchModel.php
+++ models/SearchModel.php
@@ -3,11 +3,14 @@ class SearchModel extends BaseModel
 {
   public function searchNews($search) : array
   {
-    $search = "%" . htmlspecialchars($search) . "%";
-    $statement = self::$db->prepare("SELECT * FROM news WHERE title LIKE ? OR body LIKE ?");
-    $statement->bind_param("ss", $search, $search);
-    $statement->execute();
-    $result = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+    $searchArr = explode(" ", $search);
+    $query = "SELECT * FROM news WHERE 1 != 1";
+    foreach($searchArr as $searchWord)
+    {
+      $query .= " OR title LIKE '%" . $searchWord . "%' OR body LIKE '%" . $searchWord . "%'";
+    }
+    $statement = self::$db->query($query);
+    $result = $statement->fetch_all(MYSQLI_ASSOC);
     return $result;
   }
 }
