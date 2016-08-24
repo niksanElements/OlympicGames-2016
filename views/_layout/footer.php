8e8ced17ab7a0f191a130379955c2ab24f87831d
Nikolay Nikolov <niksan9411@gmail.com>
2016-08-24 22:27:39 +0300

    make footer col-12

--- views/_layout/footer.php
+++ views/_layout/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div id="copyright">
+    <div id="copyright" class="col-lg-12 col-md-12 col-sm-12">
         <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">SoftUni</a> Fresh Team</p>
     </div>
 </footer>
