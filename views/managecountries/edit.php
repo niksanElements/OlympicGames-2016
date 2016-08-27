d95be8115e1c9dc503d739f5363b9a5c19096eb7
IgorChelikov <rogi_89@abv.bg>
2016-08-27 16:43:13 +0300

    Managecountries Complete

--- views/managecountries/edit.php
+++ views/managecountries/edit.php
@@ -6,58 +6,25 @@
                 ID:
             </th>
             <th>
-                <?=$this->countries["id"]?>
+                <?=$this->country["id"]?>
             </th>
         </tr>
         <tr>
             <th>
-                Medal name:
+                Short name:
             </th>
             <th>
-                <input type="text" name="name" value="<?=$this->medals["name"]?>" />
+                <input type="text" name="short_name" value="<?=$this->country["short_name"]?>" />
             </th>
         </tr>
         <tr>
             <th>
-                Winner
+                Full name:
             </th>
-            <th>
-                <select name="playerID">
-                    <?php foreach($this->athletes as $athlete): ?>
-                        <?php if($athlete["id"] == $this->medals["playerID"]): ?>
-                            <option value="<?=$athlete["id"]?>" selected="selected"><?=$athlete["full_name"]?></option>
-                        <?php else: ?>
-                            <option value="<?=$athlete["id"]?>"><?=$athlete["full_name"]?></option>
-                        <?php endif ?>
-                    <?php endforeach ?>
-                </select>
-            </th>
+        <th>
+            <input type="text" name="full_name" value="<?=$this->country["full_name"]?>" />
+        </th>
         </tr>
-        <tr>
-            <th>
-                Medal type:
-            </th>
-            <th>
-                <select name="type">
-                    <?php if($this->medals["type"] == 1): ?>
-                        <option value="1" selected="selected">Gold</option>
-                    <?php else: ?>
-                        <option value="1">Gold</option>
-                    <?php endif ?>
-                    <?php if($this->medals["type"] == 2): ?>
-                        <option value="2" selected="selected">Silver</option>
-                    <?php else: ?>
-                        <option value="2">Silver</option>
-                    <?php endif ?>
-                    <?php if($this->medals["type"] == 3): ?>
-                        <option value="3" selected="selected">Bronze</option>
-                    <?php else: ?>
-                        <option value="3">Bronze</option>
-                    <?php endif ?>
-                </select>
-            </th>
-        </tr>
-        <tr>
             <th colspan="2">
                 <input type="submit" name="submit-edit" value="Save" />
             </th>
