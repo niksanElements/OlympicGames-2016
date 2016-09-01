1ffe212c6aa497545251bb2d414885a29f3e881c
stefan-delchev <s.p.delchev@gmail.com>
2016-09-01 09:43:58 +0300

    fix

--- views/_layout/footer.php
+++ views/_layout/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div id="copyright" class="col-lg-12 col-md-12 col-sm-12">
-        <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">SoftUni</a> Fresh Team</p>
+        <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="https://softuni.bg/" target="_blank">SoftUni</a> Fresh Team</p>
     </div>
 </footer>
 
